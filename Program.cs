3ff1b12f8cfbe6ab0875b4d40cad62543fb25329
Poul Henriksen <pohe@easj.dk>
2020-02-14 09:19:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicrosoftArray
+{
+    class Program
+    {
+        static int[] myArray = {1, 4, 6, 2, 1, 7, 4, 6, 2, 8}; 
+
+        static int FirstUniqIterativ(int[] myArray)
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                bool foundDublicate = false; 
+                for (int j = 0; j < myArray.Length; j++)
+                {
+                    if (i != j && myArray[i] == myArray[j])
+                    {
+                        foundDublicate = true;
+                        break;
+                    }
+                }
+
+                if (!foundDublicate)
+                    return i; 
+            }
+            return -1;
+
+        }
+
+        static int FirstUniqLambda(int[] myArray)
+        {
+
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(myArray[FirstUniqIterativ(myArray)]);
+            //Console.WriteLine(myArray[FirstUniqLambda(myArray)]);
+            Console.ReadLine();
+        }
+    }
+}
